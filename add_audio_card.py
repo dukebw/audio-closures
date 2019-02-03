df03e004bfcfe08560470c261a65f9a2bd577224
Brendan Duke <brendanw.duke@gmail.com>
2019-02-03 18:02:25 -0500

    Initial commit of download/segmentation tools.

---
+++ add_audio_card.py
@@ -0,0 +1,109 @@
+# Copyright 2019 Brendan Duke.
+#
+# This file is part of Audio Closures.
+#
+# Audio Closures is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# Audio Closures is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Audio Closures. If not, see <http://www.gnu.org/licenses/>.
+
+import base64
+from glob import glob
+import os
+
+import click
+import requests
+
+
+def _anki_post(action, params=None):
+    post_json = {
+        'action': action,
+        'version': 6,
+    }
+    if params is not None:
+        post_json['params'] = params
+
+    resp = requests.post('http://localhost:8765', json=post_json)
+    assert resp.status_code == 200
+
+    resp = resp.json()
+    assert resp['error'] is None, resp['error']
+
+    return resp
+
+
+@click.command()
+@click.option('--audio-fname', type=str, default=None)
+@click.option('--func', type=click.Choice(['create', 'reset', 'sync']))
+def add_audio_card(audio_fname, func):
+    resp = _anki_post('version')
+    print(f'API version: {resp["result"]}')
+
+    if func == 'sync':
+        resp = _anki_post('sync')
+        return
+
+    resp = _anki_post('deckNames')
+    assert 'audio_closures' in resp['result']
+
+    if func == 'create':
+        front_paths = glob(f'./data/{audio_fname}_cloze*.mp4')
+        assert len(front_paths) >= 1
+        back_card = f'./data/{audio_fname}.mp4'
+        assert os.path.exists(back_card)
+
+        media_existed = False
+        for fname in front_paths + [back_card]:
+            resp = _anki_post('retrieveMediaFile', {'filename': fname})
+            if resp['result']:
+                media_existed = True
+                continue
+
+            with open(f'./data/{fname}', 'rb') as fhan:
+                raw = fhan.read()
+            audio_base64 = base64.b64encode(raw).decode('utf-8')
+            _anki_post('storeMediaFile',
+                       {'filename': fname, 'data': audio_base64})
+
+        if media_existed:
+            click.confirm('Media existed. Continue adding?', abort=True)
+
+        for front_path in front_paths:
+            front = os.path.basename(front_path)
+            front = os.path.splitext(front)[0]
+            params = {
+                'note': {
+                    'deckName': 'audio_closures',
+                    'modelName': '基本',
+                    'fields': {
+                        'Front': f'[sound:{front}.mp4]',
+                        'Back': f'[sound:{audio_fname}.mp4]'
+                    },
+                    'options': {'allowDuplicate': False},
+                    'tags': [],
+                }
+            }
+            _anki_post('addNote', params)
+
+        return
+
+    if func == 'reset':
+        click.confirm('Really reset?', abort=True)
+
+        _anki_post('deleteDecks',
+                   {'decks': ['audio_closures'], 'cardsToo': True})
+        _anki_post('createDeck', {'deck': 'audio_closures'})
+
+        return
+
+
+if __name__ == '__main__':
+    add_audio_card()  # pylint:disable=no-value-for-parameter
---
+++ segment_audio.py
@@ -0,0 +1,81 @@
+# Copyright 2019 Brendan Duke.
+#
+# This file is part of Audio Closures.
+#
+# Audio Closures is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# Audio Closures is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Audio Closures. If not, see <http://www.gnu.org/licenses/>.
+
+import json
+import os
+
+import click
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+def _to_seconds(seg):
+    seg = seg.split(':')
+    return 60*int(seg[0]) + int(seg[1])
+
+
+@click.command()
+@click.option('--youtube-id', type=str, default=None)
+def segment_audio(youtube_id):
+    if not os.path.exists(f'./data/{youtube_id}.mp4'):
+        out_fname = './data/{youtube_id}.mp4'
+        os.system(f'youtube-dl --format mp4 -o {out_fname} {youtube_id}')
+        assert os.path.exists(out_fname)
+
+    with open(f'./config/{youtube_id}.json', 'r') as fhan:
+        segs = json.load(fhan)
+
+    for seg in segs:
+        start = _to_seconds(seg[0])
+        end = _to_seconds(seg[1])
+        back_id = f'{youtube_id}_{start:08d}_{end:08d}'
+        back_fname = f'./data/{back_id}.mp4'
+        if not os.path.exists(back_fname):
+            os.system(f'ffmpeg -ss {start} -to {end} -i '
+                      f'./data/{youtube_id}.mp4 -codec copy {back_fname}')
+
+        requests.get(f'http://localhost:9090/requests/status.xml?command=in_play&input={back_fname}',
+                     auth=HTTPBasicAuth('', 'a'))
+
+        cloze_count = 0
+        while True:
+            if click.confirm('Finish segment?'):
+                break
+
+            seg_len = end - start
+            cloze_start = click.prompt('Enter cloze start', type=float)
+            assert 0 <= cloze_start <= seg_len
+
+            cloze_end = click.prompt('Enter cloze end', type=float)
+            assert 0 <= cloze_end <= seg_len
+
+            vid_cloze_path = f'./data/{back_id}_cloze{cloze_count}.mp4'
+            os.system(f'ffmpeg -i {back_fname} -af "volume=enable=\'between(t,{cloze_start},{cloze_end})\':volume=0" {vid_cloze_path}')
+
+            requests.get(f'http://localhost:9090/requests/status.xml?command=in_play&input={vid_cloze_path}',
+                         auth=HTTPBasicAuth('', 'a'))
+
+            if click.confirm('Keep cloze?'):
+                cloze_count += 1
+                audio_cloze_path = f'{os.path.splitext(vid_cloze_path)[0]}_audio.mp4'
+                os.system(f'ffmpeg -i {vid_cloze_path} -vn {audio_cloze_path}')
+
+            os.remove(vid_cloze_path)
+
+
+if __name__ == '__main__':
+    segment_audio()  # pylint:disable=no-value-for-parameter
